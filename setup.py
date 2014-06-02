2815be15f828aafdbf53ae78daa4170302bc8168
Fredrik Svensson <fredrik.svensson@mblox.com>
2014-06-02 12:38:08 +0200

    More metadata about this project

--- setup.py
+++ setup.py
@@ -9,15 +9,20 @@ setup(
     author='Fredrik Svensson',
     author_email='shootoneshot@hotmail.com',
     packages=['mixpanel'],
+    keywords='mixpanel',
     url='https://github.com/MyGGaN/mixpanel-python',
     description='Mixpanel library for Python 3',
+    platform='POSIX, MacOS, Windows, Unix',
+    license='Apache Software License',
     long_description="This library wraps Mixpanel's http API in a convenient way.  This is an in-official Mixpanel library for Python 3.",
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.4',
+        'Intended Audience :: Developers',
+        'Development Status :: 5 - Production/Stable'
     ],
     test_suite='tests'
 )
